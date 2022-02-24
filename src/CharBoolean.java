8e6ac0e4028dc1c2a5eb7cb3dde18831bce0ef3d
jigna <jigna.p@perennialsys.com>
2022-02-24 10:56:50 +0530

    upload code

---
+++ src/CharBoolean.java
@@ -0,0 +1,12 @@
+public class CharBoolean {
+    public static void  main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+    }
+}
---
+++ src/SPeedConverter.java
@@ -0,0 +1,39 @@
+public class SPeedConverter {
+    public static void main(String args[]){
+        long miles=toMilesPerHour(1.5d);
+        System.out.println(miles);
+        miles=toMilesPerHour(10.25d);
+        System.out.println(miles);
+        miles=toMilesPerHour(-5.6d);
+        System.out.println(miles);
+        miles=toMilesPerHour(25.42d);
+        System.out.println(miles);
+        miles=toMilesPerHour(75.114d);
+        System.out.println(miles);
+
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.7);
+        printConversion(25.42);
+        printConversion(75.114);
+
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour&lt;0d){
+            return-1;
+
+        }
+        return Math.round(kilometersPerHour/1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometerPerHour){
+            System.out.println("Invalid Value");
+            System.out.println("Invalid Value");
+        }else{
+            long milesPerHour=toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour+"km/h="+milesPerHour+"mi/h");
+        }
+    }
+}
